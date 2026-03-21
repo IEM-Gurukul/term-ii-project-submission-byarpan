21c39321375407d57cdda200b150066dc0b67d60
byarpan <arpanghosh8617@email.com>
2026-03-21 22:31:28 +0530

    added custom exception classes for driver not found invalid location and fare calculation errors

---
+++ src/com/rideshare/exception/InvalidLocationException.java
@@ -0,0 +1,8 @@
+package com.rideshare.exception;
+
+public class InvalidLocationException extends RuntimeException {
+
+    public InvalidLocationException(String message) {
+        super(message);
+    }
+}
